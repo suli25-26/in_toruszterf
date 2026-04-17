4bd0e99aa1a2351953f0e75b5c55ebcd04b7cb36
Erős István <eros@valahol.lan>
2026-04-17 15:03:13 +0200

    UI kész

---
+++ src/main/java/com/example/MainController.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class MainController {
+
+    @FXML
+    private TextField distanceField;
+
+    @FXML
+    private TextField radiusField;
+
+    @FXML
+    private TextField volumeField;
+
+    @FXML
+    void onClickCalcButton(ActionEvent event) {
+
+    }
+
+}
